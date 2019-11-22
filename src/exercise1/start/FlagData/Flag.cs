74d36a34dade85982c86194690a64c12acd39800
Mkhuseli Nxozana <48441802+Khuster@users.noreply.github.com>
2019-11-22 15:56:22 +0200

    DataBinding

--- src/exercise1/start/FlagData/Flag.cs
+++ src/exercise1/start/FlagData/Flag.cs
@@ -1,12 +1,22 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace FlagData
 {
     /// <summary>
     /// This model object represents a single flag
     /// </summary>
-    public class Flag
+    public class Flag : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
         /// <summary>
         /// Name of the country that this flag belongs to
         /// </summary>
@@ -18,10 +28,30 @@ namespace FlagData
         /// <summary>
         /// The date this flag was adopted
         /// </summary>
-        public DateTime DateAdopted { get; set; }
+        //  public DateTime DateAdopted { get; set; }
         /// <summary>
         /// Whether the flag includes an image/shield as part of the design
         /// </summary>
+        /// 
+
+        private DateTime _dateAdopted;
+
+        public DateTime DateAdopted
+        {
+            get { return _dateAdopted; }
+            set
+            {
+                if (_dateAdopted != value)
+                {
+                    _dateAdopted = value;
+                    // Can pass the property name as a string,
+                    // -or- let the compiler do it because of the
+                    // CallerMemberNameAttribute on the RaisePropertyChanged method.
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
         public bool IncludesShield { get; set; }
         /// <summary>
         /// Some trivia or commentary about the flag
@@ -31,5 +61,8 @@ namespace FlagData
         /// A URL for more information
         /// </summary>
         public Uri MoreInformationUrl { get; set; }
+
     }
+    
+   
 }
